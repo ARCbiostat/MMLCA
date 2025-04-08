913c431363112a33f6b9a5f53db8d024bd9780f9
caterinagregoriogit config --global user.email caterina.gregorio@outlook.com <caterina.gregorio@aldrecentrum.local>
2025-04-08 15:06:01 +0200

    added accuracy matrix

---
+++ R/get_internal_validation_matrix.R
@@ -0,0 +1,41 @@
+#' Function to calculate the approximate classification matrix
+#'
+#' @param fit object of class poLCA.
+#' @param X matrix with chronic diseases variabiables (coded as 1:no and 2:yes) to use for the calculation.
+#'
+#' @return nc x nc matrix where nc is the number of latent multimorbidity patterns.
+#' @export
+#'
+#' @examples
+get_internal_validation_matrix <- function(fit,X){
+  pClY = poLCA::poLCA.posterior(fit,y=X)
+  ng <- ncol(pClY)
+  pred <-as.numeric(apply(pClY,1,function(x)which.max(x)))# MODE
+
+
+
+  Ptable <- cbind(pClY,pred)
+  Pmatrix<-matrix(0,ng,ng)
+  Npmatrix <- matrix(0,ng,ng)
+  modclass <- pred
+
+
+  for (i in 1:ng){
+    for (j in 1:ng){
+      Pmatrix[i,j]<-sum(subset(Ptable,modclass==i)[,j])/table(modclass)[i]
+      Npmatrix[i,j]<-Pmatrix[i,j]*table(modclass)[i]
+    }}
+
+
+  denom<-colSums(Npmatrix)
+  Qmatrix<-matrix(0,ng,ng)
+
+
+  for (i in 1:ng){
+    for (j in 1:ng){
+      Qmatrix[j,i]<-Npmatrix[i,j]/denom[j]
+    }}
+
+  Qmatrix
+
+}

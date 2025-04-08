cc766d0946caec0350798cd15ab80ed19b2f7b52
caterinagregoriogit config --global user.email caterina.gregorio@outlook.com <caterina.gregorio@aldrecentrum.local>
2025-04-08 13:32:03 +0200

    apply tidyverse style

--- R/select_conditions.R
+++ R/select_conditions.R
@@ -11,17 +11,19 @@
 #' @export
 #'
 #' @examples
-#' dat <- data.frame(dis_1=rbinom(prob=0.1,size=1,n=100),dis_2=rbinom(prob=0.3,size=1,n=100),dis_3=rbinom(prob=0.05,size=1,n=100))
-#' select_conditions(dat,0.02,"dis_")
-select_conditions <- function(dat_dis,threshold, string){
-
+#' dat <- data.frame(dis_1 = rbinom(prob = 0.1, size = 1, n = 100), dis_2 = rbinom(prob = 0.3, size = 1, n = 100), dis_3 = rbinom(prob = 0.05, size = 1, n = 100))
+#' select_conditions(dat, 0.02, "dis_")
+select_conditions <- function(dat_dis, threshold, string) {
   prev <-
-    dat_dis %>% dplyr::select(contains(string)) %>%dplyr::summarise_all( function(x)
-      mean(x,na.rm = T)) %>%
+    dat_dis %>%
+    dplyr::select(contains(string)) %>%
+    dplyr::summarise_all(function(x) {
+      mean(x, na.rm = T)
+    }) %>%
     t() %>%
     as.data.frame() %>%
     dplyr::rename(prev = V1) %>%
-    dplyr::filter(prev>=threshold)
+    dplyr::filter(prev >= threshold)
 
   select_cond <- row.names(prev)
   return(select_cond)

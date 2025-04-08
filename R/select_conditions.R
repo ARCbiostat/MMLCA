cdb50d38680fc9908d063be9987b28287915c4dc
caterinagregoriogit config --global user.email caterina.gregorio@outlook.com <caterina.gregorio@aldrecentrum.local>
2025-04-08 13:03:48 +0200

    my first usable package

--- R/select_conditions.R
+++ R/select_conditions.R
@@ -1,15 +1,27 @@
-# return conditions that satisfy threshold
+#' Identify chronic conditions above threshold prevalence
+#'
+#' @param dat_dis Dataframe.
+#' @param threshold Numeric prevalence threshold to use.
+#' @param string String to identify chronic conditions in the dataset.
+#' @import magrittr
+#' @import dplyr
+#' @import tidyr
 
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' dat <- data.frame(dis_1=rbinom(prob=0.1,size=1,n=100),dis_2=rbinom(prob=0.3,size=1,n=100),dis_3=rbinom(prob=0.05,size=1,n=100))
+#' select_conditions(dat,0.02,"dis_")
 select_conditions <- function(dat_dis,threshold, string){
-  require(tidyverse)
-  require(magrittr)
+
   prev <-
-    dat_dis %>% dplyr::select(contains(string),-dis_date) %>%summarise_all( function(x)
+    dat_dis %>% dplyr::select(contains(string)) %>%dplyr::summarise_all( function(x)
       mean(x,na.rm = T)) %>%
     t() %>%
     as.data.frame() %>%
-    rename(prev = V1) %>%
-    filter(prev>=threshold)
+    dplyr::rename(prev = V1) %>%
+    dplyr::filter(prev>=threshold)
 
   select_cond <- row.names(prev)
   return(select_cond)

608340acb9b61b2d01f25698c7d5dc1fe19ed3ae
caterinagregoriogit config --global user.email caterina.gregorio@outlook.com <caterina.gregorio@aldrecentrum.local>
2025-04-09 14:39:43 +0200

    Added ggOE

--- R/ggaccuracy_LCA.R
+++ R/ggaccuracy_LCA.R
@@ -1,7 +1,7 @@
 #' Accuracy of LCA derived Multimorbidity Patterns
 #'
-#' @description This function derives and plot the accuracy of a LCA model for multimorbidity patterns using the misclassification probability and entropy on both the data used to fit the LCA and on a separate dataset.
-#' @param obj fit object of class poLCA.
+#' @description This function compares and plot the accuracy of LCA models with different number of classes for multimorbidity patterns using the misclassification probability and entropy on both the data used to fit the LCA and on a separate dataset.
+#' @param obj object returned by the function select_number_LCA.
 #' @param test Matrix with chronic diseases variables (coded as 1:no and 2:yes) used as a test set. It should be of the same format as the one used for fitting the LCA model.
 #'
 #' @return object of class ggplot.
@@ -20,7 +20,7 @@ ggaccuracy_LCA <- function(obj, test = NULL) {
     ggplot2::scale_y_continuous("Assignment accuracy (%)", limits = c(0, 1)) +
     ggplot2::scale_x_continuous("Number of latent classes", breaks = dat$nclass) +
     ggplot2::theme_bw() +
-    ggplot2::theme(axis.title = element_text(face = "bold", size = 14), axis.text = element_text(face = "bold", size = 12))
+    ggplot2::theme(axis.title = ggplot2::element_text(face = "bold", size = 14), axis.text = element_text(face = "bold", size = 12))
 
   ratio_train <- ggplot(dat) +
     ggplot2::geom_line(aes(nclass, `Assignment accuracy (%)` / prior), linewidth = 1) +
@@ -29,7 +29,7 @@ ggaccuracy_LCA <- function(obj, test = NULL) {
     ggplot2::scale_y_continuous("Ratio assignment accuracy") +
     ggplot2::scale_x_continuous("Number of latent classes", breaks = dat$nclass) +
     ggplot2::theme_bw() +
-    ggplot2::theme(axis.title = element_text(face = "bold", size = 14), axis.text = element_text(face = "bold", size = 12))
+    ggplot2::theme(axis.title = ggplot2::element_text(face = "bold", size = 14), axis.text = element_text(face = "bold", size = 12))
 
   if (!is.null(test)) {
     dat$ext_accuracy <- unlist(lapply(1:nrow(dat), function(x) mean(diag(get_internal_validation_matrix(obj$obj[[x]], test)))))
@@ -42,7 +42,8 @@ ggaccuracy_LCA <- function(obj, test = NULL) {
       ggplot2::scale_y_continuous("Assignment accuracy (%)", limits = c(0, 1)) +
       ggplot2::scale_x_continuous("Number of latent classes", breaks = dat$nclass) +
       ggplot2::theme_bw() +
-      ggplot2::theme(axis.title = element_text(face = "bold", size = 14), axis.text = element_text(face = "bold", size = 12))
+      ggplot2::scale_color_discrete("")+
+      ggplot2::theme(axis.title = ggplot2::element_text(face = "bold", size = 14), axis.text = element_text(face = "bold", size = 12))
 
 
     ratio_test <- ggplot2::ggplot(dat) +
@@ -53,7 +54,8 @@ ggaccuracy_LCA <- function(obj, test = NULL) {
       ggplot2::scale_y_continuous("Ratio assignment accuracy") +
       ggplot2::scale_x_continuous("Number of latent classes", breaks = dat$nclass) +
       ggplot2::theme_bw() +
-      ggplot2::theme(axis.title = element_text(face = "bold", size = 14), axis.text = element_text(face = "bold", size = 12))
+      ggplot2::scale_color_discrete("")+
+      ggplot2::theme(axis.title = ggplot2::element_text(face = "bold", size = 14), axis.text = element_text(face = "bold", size = 12))
     gg <- ggpubr::ggarrange(gg_test, ratio_test, ncol = 2, common.legend = T)
   } else {
     gg <- ggpubr::ggarrange(gg_train, ratio_train, ncol = 2, common.legend = T)

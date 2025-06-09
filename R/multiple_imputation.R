a3d484de212e0d7104c4fb4388e171fe507378b5
Caterina Gregorio <caterina.gregorio@outlook.com>
2025-06-09 10:26:30 +0200

    modified imputation function

--- R/multiple_imputation.R
+++ R/multiple_imputation.R
@@ -1,7 +1,7 @@
 #' Function to obtain imputate multiple imputations of the assigned MM patterns
 #'
 #' @param obj LCA object
-#' @param data
+#' @param data your dataset
 #' @param nimp
 #'
 #' @return
@@ -9,6 +9,14 @@
 #'
 #' @examples
 multiple_imputation <- function(obj, data, nimp) {
+
+  X <- data %>% dplyr::select(dplyr::any_of(colnames(obj$y)))
+  if (ncol(X)!=length(colnames(obj$y))) stop("Some disease columns are missing!")
+  lev <- unique(unlist(lapply(1:ncol(X), function(y) unique(X[, y]))))
+  if (NA %in% lev) stop("Disease columns cannot contain missing values.")
+  if (!all(lev %in% c(0, 1)) | any(sapply(lev, is.character))) stop("Disease columns must be numeric 0/1 variables.")
+  X %<>% dplyr::mutate_all(function(x) x + 1)
+
   data_imp <- list()
   pClY <- poLCA::poLCA.posterior(obj, y = X)
   ng <- ncol(pClY)

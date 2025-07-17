5be921c4796920a6d46f533b99cede226fbb15dd
Caterina Gregorio <caterina.gregorio@outlook.com>
2025-07-17 11:40:24 +0200

    bug

--- R/select_number_LCA.R
+++ R/select_number_LCA.R
@@ -19,8 +19,8 @@ select_number_LCA <- function(nclasses, X, conditions, plot = T, nrep = 50,cvfol
   tictoc::tic()
   if(!is.null(cvfolds)){
   plan(multisession)
-  folds <- sample(rep(1:nfolds, length.out = nrow(X)))
-  results <- do.call("rbind",future_lapply(1:nfolds, run_lca_cv))
+  folds <- sample(rep(1:cvfolds, length.out = nrow(X)))
+  results <- do.call("rbind",future_lapply(1:cvfolds, run_lca_cv))
   results %<>% group_by(nclass) %>% summarise_all(mean) %>% select(-CV)
 
   if (plot) {

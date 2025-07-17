fb3238de68a78fbab9195af221bbb19b8f0f881d
Caterina Gregorio <caterina.gregorio@outlook.com>
2025-07-17 22:54:47 +0200

    buug

--- R/run_lca_cv.R
+++ R/run_lca_cv.R
@@ -2,7 +2,7 @@ run_lca_cv <- function(fold,nclasses,X,conditions,nrep){
   res <- lapply(nclasses, function(x) run_LCA(x, X = X[-fold,], conditions = conditions, nrep = nrep))
   objects <- lapply(res, function(x) x$obj)
   names(objects) <- nclasses
-  entropy <- unlist(lapply(1:length(objects), function(x) function(x)get_entropy(objects[[x]],X[fold,],ratio=F)))
+  entropy <- unlist(lapply(1:length(objects), function(x)get_entropy(objects[[x]],X[fold,],ratio=F)))
   names(entropy) <- nclasses
   ext_accuracy <- unlist(lapply(1:length(objects), function(x) sum(diag(get_internal_validation_matrix(objects[[x]], X[fold,])))/nclasses[x]))
   dat_res <- data.frame(cv=fold,

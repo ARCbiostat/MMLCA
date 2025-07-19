b4fc324cb6ac76c9d501a4a3c6408854620ceee1
Caterina Gregorio <caterina.gregorio@outlook.com>
2025-07-19 09:22:16 +0200

    ratio

--- R/run_lca_cv.R
+++ R/run_lca_cv.R
@@ -2,7 +2,7 @@ run_lca_cv <- function(fold,nclasses,X,conditions,nrep,folds){
   res <- lapply(nclasses, function(x) run_LCA(x, X = X[-(folds==fold),], conditions = conditions, nrep = nrep))
   objects <- lapply(res, function(x) x$obj)
   names(objects) <- nclasses
-  entropy <- unlist(lapply(1:length(objects), function(x)get_entropy(objects[[x]],X[(folds==fold),],ratio=F)))
+  entropy <- unlist(lapply(1:length(objects), function(x)get_entropy(objects[[x]],X[(folds==fold),],ratio=T)))
   names(entropy) <- nclasses
   ext_accuracy <- unlist(lapply(1:length(objects), function(x) sum(diag(get_internal_validation_matrix(objects[[x]], X[(folds==fold),])))/nclasses[x]))
   dat_res <- data.frame(cv=fold,
